60c8f7f1150de2a3b3332b7c8aba64d76cab2ae0
Holly <hlavery10@qub.ac.uk>
2024-11-27 12:18:51 +0000

    added testing

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,6 +7,7 @@ import pytest
 from inflammation.models import daily_mean
 from inflammation.models import daily_max
 from inflammation.models import daily_min
+from inflammation.models import patient_normalise
 
 @pytest.mark.parametrize(
     "test, expected",
@@ -57,10 +58,35 @@ def test_daily_min_integers():
     npt.assert_array_equal(daily_min(test_input), test_result)
 
 
-
-
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        ),
+    ])
+
+def test_patient_normalise(test , expected , expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            result = patient_normalise(np.array(test))
+            npt.assert_allclose(result , np.array(expected) , rtol=1e-2 , atol=1e-2)
+    else:
+        result = patient_normalise(np.array(test))
+        npt.assert_allclose(result , np.array(expected) , rtol=1e-2 , atol=1e-2)
